6ee7c40183b81853e3afcb503b66c8939d4fb9cf
Dimitris Sidiropoulos <jimsid@hotmail.gr>
2026-05-13 14:47:32 +0300

    optimize FastF1 session loading to reduce memory usage

--- app/utils.py
+++ app/utils.py
@@ -2,5 +2,11 @@ import fastf1
 
 def get_session(year, rnd, session_type):
     session = fastf1.get_session(year, rnd, session_type.upper())
-    session.load()
+    session.load(
+        laps=True,
+        telemetry=True,
+        weather=True,
+        messages=False,
+        livedata=None
+    )
     return session
